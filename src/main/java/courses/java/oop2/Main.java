c543ea028cb6a920e18570b954af7f929335cbaa
Daniel <dany_dany_92_2007@yahoo.com>
2024-08-19 14:06:31 +0200

    added loombak

---
+++ src/main/java/courses/java/oop2/Main.java
@@ -0,0 +1,9 @@
+package courses.java.oop2;
+
+public class Main {
+    public static void main(String[] args) {
+    Shape s1 = new Shape("fucshia");
+    s1.setColor("red");
+        System.out.println("The color of the shape is " + s1.getColor());
+    }
+}
